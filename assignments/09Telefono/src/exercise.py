d2fd3d2af6b031c5612a80bcf365ca5e4f7ff91d
TLuisEduardo <a01411855@itesm.mx>
2021-08-27 11:00:10 -0700

    tarea 1

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+
+    mensajes = int(input("Dame el numero de mensajes "))
+    megas = float(input("Dame el numero de megas "))
+    minutos = int(input("Dame el numero de minutos "))
 
 
 if __name__ == '__main__':
--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,7 +1,15 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
 
+    jnuevos = int(input("Dame la cantidad de juegos nuevos "))
+    jusados = int(input("Dame la cantidad de juegos usados "))
+
+    totaljnuevos = jnuevos * 1000
+    totaljusados = jusados * 350
+
+    totalcompra= totaljnuevos + totaljusados
+
+    print("El total de precio es", totalcompra)
 
 if __name__ == '__main__':
     main()
