7d0eae6913cebfe805ade058f2964f0a1a3b550d
TLuisEduardo <a01411855@itesm.mx>
2021-08-29 12:07:38 -0700

    tarea 1

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -2,6 +2,19 @@ def main():
     # escribe tu código abajo de esta línea
     pass
 
+    palabras = int(input("Dame el numero de palabras "))
+
+    paginas = round((palabras / 450+1))
+
+
+    print(paginas)
+
+    costo = (paginas * 54)
+
+    print("El costo de la publicación es de", costo)
+
+
+
 
 if __name__ == '__main__':
     main()
